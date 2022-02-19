f29d0d154c8ea9ee95ae5042d3dc71e8386f8c14
dasom <dasom228@gmail.com>
2022-02-19 23:50:10 +0900

    Add minting view

--- src/routes/main.tsx
+++ src/routes/main.tsx
@@ -1,32 +1,26 @@
 import React, { FC, useEffect, useState } from "react";
 import { Box, Text, Flex, Button } from "@chakra-ui/react";
 
-const Main: FC = () => {
-  const [account, setAccount] = useState<string>("")
-  const getAccount = async () => {
-    try {
-      if (window.ethereum) { // metamask설치되어 있는경우
-        // 브라우저에서 metamask 연결 요청하여 account배열 get
-        const accounts = await window.ethereum.request({
-          method: 'eth_requestAccounts'
-        })
-        setAccount(accounts[0])
-        return
-      }
-      alert('Install Meta Mask!') // metamask 설치되지 않은 경우
-    } catch(error) {
-      console.error(error)
-    }
+interface MainProps {
+  account: string
+}
+
+const Main: FC<MainProps> = ({ account }) => {
+  const [newAnimalCard, setAnimalCard] = useState<string>('')
+  const handleMint = () => {
+    console.log('click!!')
   }
 
-  useEffect(() => {
-    getAccount()
-  }, [])
-  useEffect(() => {
-    console.log('account', account)
-  }, [account])
-
-  return <Box backgroundColor="blue" color="white">Main page</Box>
+  return (
+    <>
+      {newAnimalCard ?
+        <div>Animal card</div> 
+        : <Box>
+            <Text>Let's mint animal card!!</Text>
+          </Box>}
+      <Button size="sm" colorScheme={"lightpink"} onClick={handleMint}>Mint</Button>
+    </>
+  )
 }
 
 export default Main
