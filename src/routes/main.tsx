fa7e0958772bf4b7043805e500df8523ac762404
dasom <dasom228@gmail.com>
2022-02-19 23:16:18 +0900

    Add getAccount

--- src/routes/main.tsx
+++ src/routes/main.tsx
@@ -2,6 +2,30 @@ import React, { FC, useEffect, useState } from "react";
 import { Box, Text, Flex, Button } from "@chakra-ui/react";
 
 const Main: FC = () => {
+  const [account, setAccount] = useState<string>("")
+  const getAccount = async () => {
+    try {
+      if (window.ethereum) { // metamask설치되어 있는경우
+        // 브라우저에서 metamask 연결 요청하여 account배열 get
+        const accounts = await window.ethereum.request({
+          method: 'eth_requestAccounts'
+        })
+        setAccount(accounts[0])
+        return
+      }
+      alert('Install Meta Mask!') // metamask 설치되지 않은 경우
+    } catch(error) {
+      console.error(error)
+    }
+  }
+
+  useEffect(() => {
+    getAccount()
+  }, [])
+  useEffect(() => {
+    console.log('account', account)
+  }, [account])
+
   return <Box backgroundColor="blue" color="white">Main page</Box>
 }
 
