0f8ceb194c2f04325a17c8fa7e0f39cc1c27e8e0
dasom <dasom228@gmail.com>
2022-02-20 22:25:45 +0900

    Set input value

--- src/components/MyAnimalCard.tsx
+++ src/components/MyAnimalCard.tsx
@@ -1,20 +1,48 @@
 import React, { FC } from 'react'
 import { IAnimalCard } from '../lib/typs'
 import AnimalCard from './AnimalCard'
-import { Box, Text } from '@chakra-ui/react'
+import useInputs from '../hooks/useInputs'
+import { Box, Button, Input, InputAddon, InputGroup, Text } from '@chakra-ui/react'
 
 interface MyAnimalCardProps extends IAnimalCard {
   account: string
   isSaleStatus: boolean
 }
 
-const MyAnimalCard: FC<MyAnimalCardProps> = ({ account, isSaleStatus, animalTokenId, animalType, animalPrice }) => {
+const MyAnimalCard: FC<MyAnimalCardProps> = ({
+  account,
+  isSaleStatus,
+  animalTokenId,
+  animalType,
+  animalPrice,
+}) => {
+  const initial = { price: 0 }
+  const [form, onChange] = useInputs(initial)
+  const { price } = form
+
+  const handleSell = () => {
+    console.log('price', price)
+  }
+
   return (
-    <Box w="150" textAlign="center">
+    <Box w={150} textAlign="center">
       <AnimalCard animalType={animalType} />
-      <Text>{animalPrice === 0 ? '판매 버튼' : `${animalPrice} Matic`}</Text>
+      <Box>
+        {animalPrice === 0 ? (
+          <>
+            <InputGroup size="sm">
+              <Input type="number" name="price" value={price} onChange={onChange} />
+              <InputAddon children="Matic" />
+            </InputGroup>
+            <Button colorScheme="blue" mt={2} onClick={handleSell}>Sell</Button>
+          </>
+        ) : (
+          `${animalPrice} Matic`
+        )}
+      </Box>
     </Box>
   )
 }
 
 export default MyAnimalCard
+
