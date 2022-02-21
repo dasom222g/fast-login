8048f189deb194d54b1d83ada4e92474de6707e7
dasom <dasom228@gmail.com>
2022-02-21 23:40:48 +0900

    Add saleAnimalTokenLength

--- src/routes/sale-animal.tsx
+++ src/routes/sale-animal.tsx
@@ -1,10 +1,24 @@
-import React, { FC } from 'react'
+import React, { FC, useEffect } from 'react'
+import { saleAnimalTokenContract } from '../web3Config'
 
 interface SaleAnimalProps {
   account: string
 }
 
 const SaleAnimal: FC<SaleAnimalProps> = ({ account }) => {
+  const getOnSaleAnimalToken = async () => {
+    try {
+      const saleAnimalTokenLength: string = await saleAnimalTokenContract.methods.getOnSaleAnimalTokenLength().call()
+      console.log('saleAnimalTokenLength', saleAnimalTokenLength)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  useEffect(() => {
+    account && getOnSaleAnimalToken()
+  }, [account])
+  // view
   return (
     <div>SaleAnimal</div>
   )
